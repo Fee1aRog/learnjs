c2d5fae517724c5924c519172299424ace17d651
George <barishvili2001@gmail.com>
2021-11-04 13:39:58 +0400

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+var k = 6;
+if ( k > 5 ) {
+    console.log('this number is more than 5')
+}else if (k < 5){
+    console.log('this number is less than 5')
+}else {
+    console.log('this number equals 5')
+}
