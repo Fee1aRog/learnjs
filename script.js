d4eded31578a327b4e04ecfeb0fdeaded70d50df
George <barishvili2001@gmail.com>
2021-11-04 14:27:17 +0400

    l

--- script.js
+++ script.js
@@ -1,8 +1,150 @@
-var k = 6;
-if ( k > 5 ) {
-    console.log('this number is more than 5')
-}else if (k < 5){
-    console.log('this number is less than 5')
-}else {
-    console.log('this number equals 5')
-}
+// var k = 6;
+// if ( k > 5 ) {
+//     console.log('this number is more than 5')
+// }else if (k < 5){
+//     console.log('this number is less than 5')
+// }else {
+//     console.log('this number equals 5')
+// }
+
+// var hours = 9;
+// var weekend = false;
+// if (hours < 10 && hours > 8 && weekend){
+//     console.log('test')
+// }else{
+//     console.log('nothing')
+// }
+
+//&&-"da (and)------- ||-"an"(or) */
+// || true   true   = true
+// || true   false   = true
+// || false  true   = true
+// || false  false  = false
+
+// && true true = true
+// && false true =  false
+// && true false  = false
+// && false false  =  false
+
+// var fruit  = "Banana"
+// switch (fruit) {
+//     case "Banana" :
+//         console.log('This is Banana');
+//         break;
+//     case "Apple" :
+//         console.log('this is Apple');
+//         break;
+//     case "Kiwi" :
+//         console.log('this is kiwi')
+//         break;
+//     case "Cherry":
+//         console.log('this is cherry')
+//         break
+//     default:
+//         console.log('this fruit is not defined')
+// }
+
+// if (true) {
+//     let  test = 123;
+//     console.log(test)
+// }
+
+// console.log(test)
+
+
+// const test = 23;
+// console.log(test)
+
+// test = 50;
+// console.log(test)
+
+// let obj = {
+//     name: "zhana",
+//     surname: "jikia"
+// }
+
+// obj.surname === 'smith'
+// console.log(obj)
+
+// var number = [1, 2, 3, 5, 2.5, 6, 8, 9];
+// // console.log(number.length)
+// for (var i = 0; i <= number.length; i++ ){
+//     // console.log(i)
+//     if ( number[i] >= 5) {
+//         console.log(number[i])
+//     }
+// }
+
+// number.forEach( function (elem,index) {
+//     console.log(elem + " - " + index)
+// })
+
+// var fruit = ['Banana', 'cherry','Apple'];
+// console.log(fruit)
+// fruit.shift()
+// fruit.push('kiwi')
+// fruit.pop()
+// var c = fruit.slice(0,1);
+// console.log(c)
+// console.log(fruit)
+
+// var post = {
+//     title : 'post',
+//     comment: 'comments comment',
+//     content: [
+//         { 
+//             id: 1,
+//             text: 'test text',
+//             info: [
+//                 {
+//                     number: "5*******",
+//                     postalCode: 1234
+//                 },
+//                 {
+//                     number: "+995*******",
+//                     postalCode: 3456
+//                 }
+//             ]
+//         },
+//         { 
+//             id: 2,
+//             text: 'test text 2'
+//         }
+//     ]
+// }
+
+// console.log(post.content[1].text)
+// console.log(post.content[0].info[1])
+
+// var obj = {
+//     name: 'Tea',
+//     age : 25,
+//     arr : [
+//         {
+//             id: 2,
+//             age1: 60
+//         },
+//         {
+//             id: 2,
+//             age1: 50
+//         },
+//         {
+//             id: 2,
+//             age1: 90
+//         }
+//     ]
+// }
+
+// console.log(obj.arr[0].age1)
+
+// var persons  = {
+//     name : 'Gio',
+//     age: 20,
+//     email : 'gio@gmail.com'
+// }
+
+
+
+// for (var person in persons){
+//     console.log(person)
+// }
